b5c5be63412c352deac0994972ebb722bc5126a4
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-08-27 15:38:02 +0100

    layout mobile

--- src/components/MadeIn/styles.ts
+++ src/components/MadeIn/styles.ts
@@ -4,6 +4,11 @@ export const Container = styled.div`
   text-align: center;
   padding: 8px;
   z-index: 0;
+  margin-top: 6rem;
+
+  @media only screen and (min-width: 1024px) {
+    margin-top: 0;
+  }
 `
 
 export const Stamp = styled.h1`
@@ -11,8 +16,13 @@ export const Stamp = styled.h1`
     font-family: 'Bodedo';
     src: url('HVD_Bodedo.ttf');
   }
-  font-size: 4.5rem;
+
+  font-size: 3.5rem;
   font-weight: 100;
   font-family: Bodedo, Roboto, sans-serif;
   text-shadow: 1px 1px 1px #000;
+
+  @media only screen and (min-width: 1024px) {
+    font-size: 4.5rem;
+  }
 `
