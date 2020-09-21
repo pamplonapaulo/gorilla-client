3035b9b3bc6c03215b1e56c656262637d11e0ef2
Paulo Pamplona <pamplonapaulo@gmail.com>
2020-09-21 19:23:49 +0100

    products display started

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,7 +13,7 @@ const GlobalStyles = createGlobalStyle`
 
   html, body, #__next {
     height: 100%;
-    overflow: hidden
+    overflow: hidden;
 
     -ms-overflow-style: none;
     scrollbar-width: none;
