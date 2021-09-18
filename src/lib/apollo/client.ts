8c6dd7c9e3a0bbb9b93407a91fe98d8ebd7c6f5f
PAULO PAMPLONA <paulopamplona@PAULOs-MacBook-Pro.local>
2021-09-18 18:22:17 -0300

    still trying to fix bug on _document.tsx

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 // export const endpoint = 'http://localhost:1337/'
-export const endpoint = 'https://packgorilla.herokuapp.com/'
+// export const endpoint = 'https://packgorilla.herokuapp.com/'
+export const endpoint = 'http://142.93.139.20/'
 
 export const client = new ApolloClient({
   uri: endpoint + 'graphql',
